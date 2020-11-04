44cd1e8c13bd7da9282dc40138ff3090725ff398
brechtwillems99 <58217092+brechtwillems99@users.noreply.github.com>
2020-11-04 14:20:50 +0100

    Add files via upload

---
+++ DSChatroomRMIClient/src/client/ClientInterface.java
@@ -0,0 +1,15 @@
+package client;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+
+
+public interface ClientInterface extends Remote{
+	
+	public String getName() throws RemoteException;
+	public void receiveMessage(String s) throws RemoteException;
+	
+	
+
+}
